cd48b7c2b5af449bc721de1c6c025579bf3a5077
Elwyn <gc92@mail.ru>
2022-02-17 19:19:15 +0300

    add script

---
+++ scripts/index.js
@@ -0,0 +1,40 @@
+"use strict";
+
+
+const container = document.querySelector('.profile__container');
+const popup = document.querySelector('.popup');
+const editButton = container.querySelector('.profile__edit-button');
+const popupForm = popup.querySelector('.popup__form');
+let closeButton = popup.querySelector('.popup__close');
+
+
+
+let nameInput = popup.querySelector('.popup__input_name');
+let jobInput = popup.querySelector('.popup__input_profession');
+let nameProfile = container.querySelector('.profile__title');
+let jobProfile = container.querySelector('.profile__subtitle');
+
+
+function popupOpened() {
+    popup.classList.add('popup_opened');
+    nameInput.value = nameProfile.textContent;
+    jobInput.value = jobProfile.textContent;
+}
+
+function popupClosed() {
+    popup.classList.remove('popup_opened');
+}
+
+
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+
+    nameProfile.textContent = nameInput.value;
+    jobProfile.textContent = jobInput.value;
+    popupClosed();
+
+}
+
+editButton.addEventListener('click', popupOpened);
+closeButton.addEventListener('click', popupClosed);
+popupForm.addEventListener('submit', formSubmitHandler);
