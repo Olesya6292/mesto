2b70bcb80b196348478858eb3ca9ae171a75156f
Elwyn <gc92@mail.ru>
2022-02-20 15:55:31 +0300

    после 1-го ревью

--- scripts/index.js
+++ scripts/index.js
@@ -3,27 +3,25 @@
 const container = document.querySelector('.profile__container');
 const popup = document.querySelector('.popup');
 const editButton = container.querySelector('.profile__edit-button');
-const popupForm = popup.querySelector('.popup__form');
+const popupForm = popup.querySelector('.popup__name');
 const closeButton = popup.querySelector('.popup__close');
 
 
 
-let nameInput = popup.querySelector('.popup__input_name');
-let jobInput = popup.querySelector('.popup__input_profession');
+let nameInput = popup.querySelector('.popup__input_type_name');
+let jobInput = popup.querySelector('.popup__input_type_profession');
 let nameProfile = container.querySelector('.profile__title');
 let jobProfile = container.querySelector('.profile__subtitle');
 
 
 function popupOpened() {
     popup.classList.add('popup_opened');
-    document.body.classList.add('page_disabled');
     nameInput.value = nameProfile.textContent;
     jobInput.value = jobProfile.textContent;
 }
 
 function popupClosed() {
     popup.classList.remove('popup_opened');
-    document.body.classList.remove('page_disabled');
 }
 
 
