0eb0b871d7cbb2041f797d9f6c1245c4b7c292f5
Elwyn <gc92@mail.ru>
2022-02-21 10:26:15 +0300

    исправления после 2-го ревью

--- scripts/index.js
+++ scripts/index.js
@@ -5,19 +5,16 @@ const popup = document.querySelector('.popup');
 const editButton = container.querySelector('.profile__edit-button');
 const popupForm = popup.querySelector('.popup__name');
 const closeButton = popup.querySelector('.popup__close');
-
-
-
-let nameInput = popup.querySelector('.popup__input_type_name');
-let jobInput = popup.querySelector('.popup__input_type_profession');
-let nameProfile = container.querySelector('.profile__title');
-let jobProfile = container.querySelector('.profile__subtitle');
+const nameInput = popup.querySelector('.popup__input_type_name');
+const jobInput = popup.querySelector('.popup__input_type_profession');
+const nameProfile = container.querySelector('.profile__title');
+const jobProfile = container.querySelector('.profile__subtitle');
 
 
 function popupOpened() {
-    popup.classList.add('popup_opened');
     nameInput.value = nameProfile.textContent;
     jobInput.value = jobProfile.textContent;
+    popup.classList.add('popup_opened');
 }
 
 function popupClosed() {
@@ -27,7 +24,6 @@ function popupClosed() {
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
-
     nameProfile.textContent = nameInput.value;
     jobProfile.textContent = jobInput.value;
     popupClosed();
